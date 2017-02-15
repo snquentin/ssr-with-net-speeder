559c8adfefe5ce2c6a8bf4a3de7c986c7cfd4c49
malaohu <re_ming.ma@hotmail.com>
2017-02-15 17:46:36 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var superagent = require('superagent');
+var CronJob = require('cron').CronJob;
+//1小时半执行一次
+new CronJob('* 30 1 * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
